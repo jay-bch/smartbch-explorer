2041e992fc7249a212e81b21b5d3cdceb76827d1
Jay BCH <jaydizzzbch@gmail.com>
2021-05-15 00:26:46 +0200

    update to angular 12

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -34,6 +34,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDialogModule } from '@angular/material/dialog'
 
 const materialModules = [
   CdkTreeModule,
@@ -69,7 +70,8 @@ const materialModules = [
   MatDatepickerModule,
   MatTooltipModule,
   MatSlideToggleModule,
-  MatProgressBarModule
+  MatProgressBarModule,
+  MatDialogModule
 ];
 
 @NgModule({
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -57,9 +57,12 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+ (window as any).global = window;
+ global.Buffer = global.Buffer || require('buffer').Buffer;
+ global.process = require('process');
