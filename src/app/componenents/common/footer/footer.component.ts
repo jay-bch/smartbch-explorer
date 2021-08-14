f8fc0c4ce48812cb963c73197289742eca9f9a9c
Jay BCH <jaydizzzbch@gmail.com>
2021-08-15 00:29:38 +0200

    feat(footer): add version

--- src/app/componenents/common/footer/footer.component.ts
+++ src/app/componenents/common/footer/footer.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import packageSettings from '../../../../../package.json';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  version: string;
 
-  constructor() { }
+  constructor() {
+    this.version = packageSettings.version;
+  }
 
   ngOnInit(): void {
   }
