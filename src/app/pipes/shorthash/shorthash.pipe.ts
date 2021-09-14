4e693504cda7a6e552b64ad2b65c66d4cb8a1470
Jay BCH <jaydizzzbch@gmail.com>
2021-09-14 21:53:30 +0200

    fix(hash-pipe): shorter short hash

--- src/app/pipes/shorthash/shorthash.pipe.ts
+++ src/app/pipes/shorthash/shorthash.pipe.ts
@@ -7,7 +7,7 @@ export class ShorthashPipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]): unknown {
     if(value && value.startsWith('0x')) {
-      return `${value.substring(0, 10)}...${value.substring(value.length - 10, value.length)}`;
+      return `${value.substring(0, 7)}...${value.substring(value.length - 7, value.length)}`;
     }
 
     return value;
